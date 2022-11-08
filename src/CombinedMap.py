d09a043875b82e7ca5702288eff98f299b35cb9f
h3h3h0h0 <deboss.wt@gmail.com>
2022-11-08 11:20:40 -0500

    graph theory (will finish later)

--- src/CombinedMap.py
+++ src/CombinedMap.py
@@ -1,3 +1,5 @@
+import current as current
+
 import OSMHandler
 import GISGrabber
 from copy import deepcopy
@@ -24,9 +26,38 @@ class CombinedMap:
 
     #makes a route based on a set of points
     def makeRoute(self, ids):
+        #need more than one point to make a valid route
+        if len(ids) < 2:
+            raise Exception("Path needs more than 1 point!")
+
+        #various helper variables for BFS and backtracking
         route = []
         length = 0
         l = [0 for i in range(len(self.roadGraph.adj))]
         vis = [False for i in range(len(self.roadGraph.adj))]
+        parent = [None for i in range(len(self.roadGraph.adj))]
         q = queue.Queue()
+
+        #current start/end (for each section between 2 points)
+        cs = self.roadGraph.idToNum[ids[0]]
+        ce = self.roadGraph.idToNum[ids[1]]
+        vis[cs] = True
+        q.put(cs)
+
+        for sec in range(1, len(self.roadGraph.adj)):
+            while not q.empty():
+                cur = q.get()
+                for i in self.roadGraph.adj[cur].keys():
+                    if (not i in route) and ((not vis[i]) or l[i] > l[cur]+self.roadGraph.adj[cur][i][0]):
+                        parent[i] = cur
+                        l[i] = l[cur]+self.roadGraph.adj[cur][i][0]
+                        vis[i] = True
+                        q.put(i)
+
+            length += l[ce]
+            curRoute = []
+            curN = ce
+            while True:
+                curRoute.insert(0, curN)
+
 
